b42959ef23c0788cc0566ebab972e96b501241de
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-03 14:52:36 -0300

    Initial commit

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss'],
+})
+export class PokeSearchComponent implements OnInit {
+  @Output() public emitSearch: EventEmitter<string> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public search(value: string) {
+    this.emitSearch.emit(value);
+  }
+}
